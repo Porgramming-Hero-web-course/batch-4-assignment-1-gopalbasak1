a17a6df0d0474f43b6870ae507020708078afb8a
gopalbasak1 <gopalbasak1223@gmail.com>
2024-11-07 15:28:44 +0600

    tsc file setup and 1st problem slove

---
+++ BLOG-2.md
@@ -0,0 +1,1 @@
+Solve the problems in a separate file. Name the file starting with "problem-" followed by the problem number. (For example: problem-1.ts)
---
+++ assignment/src/problem-2.ts
@@ -0,0 +1,12 @@
+{
+    //
+        function sumArray(numbers: number[]): number {
+        return numbers.reduce((accumulator, current) => accumulator + current, 0);
+    }
+    
+    // Sample Input
+    console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15
+    
+    
+    //
+    }
